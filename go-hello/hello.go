80ec369ea6a49ea2d19fb95c870c7f3b8b4c7870
Fred Thiele <fthiele@intershop.de>
2015-10-14 17:33:17 +0200

    added new function / tried out liteide

--- go-hello/hello.go
+++ go-hello/hello.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-  "fmt"
-  sm "github.com/f3rdy/go-test/go-simplemath"
+	"fmt"
+
+	sm "github.com/f3rdy/go-test/go-simplemath"
 )
 
+
+
 func main() {
-  fmt.Printf("1+1=%#v\n", sm.Add(1,1))
-  fmt.Printf("Hello, World!\n")
+	fmt.Printf("1+1=%d\n", sm.Add(1, 1))
+	fmt.Printf("5-3=%d\n", sm.Sub(5,3))
+	fmt.Printf("Hello, World!\n")
 }
--- go-simplemath/simple_test.go
+++ go-simplemath/simple_test.go
@@ -3,18 +3,35 @@ package simplemath
 import "testing"
 
 func TestAdd(t *testing.T) {
-  cases := []struct {
-    in_a, in_b, want int
-  }{
-    {1,1,2},
-    {3,3,6},
-    {0,1,1},
-    {5,5,10},
-  }
-  for _, c:= range cases {
-    got := Add(c.in_a, c.in_b)
-    if got != c.want {
-      t.Errorf("Add(%d,%d) == %d, want %d", c.in_a, c.in_b, got, c.want)
-    }
-  }
+	cases := []struct {
+		in_a, in_b, want int
+	}{
+		{1, 1, 2},
+		{3, 3, 6},
+		{0, 1, 1},
+		{5, 5, 10},
+	}
+	for _, c := range cases {
+		got := Add(c.in_a, c.in_b)
+		if got != c.want {
+			t.Errorf("Add(%d,%d) == %d, want %d", c.in_a, c.in_b, got, c.want)
+		}
+	}
+}
+
+func TestSub(t *testing.T) {
+	cases := []struct {
+		in_a, in_b, want int
+	}{
+		{2,1,1},
+		{5,1,4},
+		{7,5,2},
+	}
+	for _, c := range cases {
+		got := Sub(c.in_a, c.in_b)
+		if got != c.want {
+			t.Errorf("Sub(%d,%d) == %d, want %d", c.in_a, c.in_b, got, c.want)
+		}
+	}
+	
 }
